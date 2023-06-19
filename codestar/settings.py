30af211188e8aa2d4d72bc739bb3a98525b66ea9
Dhvani-intwala <intdhvani2627@gmail.com>
2023-06-19 08:30:16 +0000

    upated

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,11 +31,12 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ['sweet-and-spicy.herokuapp.com', 'localhost']
+# ALLOWED_HOSTS = ['sweet-and-spicy.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
